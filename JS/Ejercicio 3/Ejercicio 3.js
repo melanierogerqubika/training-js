d00b18688e3a7713bf9c357da8af78b49f99eddf
Gaston <gaston.medina@decemberlabs.com>
2024-06-17 21:43:06 -0300

    ejercicios 2 y 3

---
+++ JS/Ejercicio 3/Ejercicio 3.js
@@ -0,0 +1,10 @@
+// Objetivo
+// Crear una jerarquía de clases que simule un sistema de vehículos, utilizando la herencia para compartir y extender funcionalidades.
+
+// Instrucciones
+// Define una clase Vehiculo: Esta clase debe tener propiedades como marca y modelo, y un método mostrarDetalles
+// que imprima las propiedades del vehículo.
+// Crea una subclase Coche: Extiende la clase Vehiculo e incluye una propiedad adicional tipoCombustible.
+// Sobrescribe el método mostrarDetalles para incluir esta nueva propiedad.
+// Crea una subclase Bicicleta: Extiende la clase Vehiculo e incluye una propiedad adicional tipoBicicleta.
+// Sobrescribe el método mostrarDetalles para incluir esta nueva propiedad.
